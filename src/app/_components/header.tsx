0cb4168bed5d065702762ee914096a53bd5125fc
Linda Paz <Lindacsndelapaz@gmail.com>
2024-11-09 15:53:35 -0300

    feat: prove react-responsive and use in desktop mode size

--- src/app/_components/header.tsx
+++ src/app/_components/header.tsx
@@ -1,4 +1,28 @@
+"use client";
+
+import { useEffect, useState } from "react";
+
+// next image
+// import Image from "next/image";
+
+// LocomotiveScroll
+// import LocomotiveScroll from 'locomotive-scroll';
+import "locomotive-scroll/dist/locomotive-scroll.css";
+
+// media queries
+import { useMediaQuery } from "react-responsive";
+
 const HomeHeader = () => {
+  const [header, setHeader] = useState(false);
+  const [nav, setNav] = useState(false);
+
+  // Ancho de la pantalla
+  const destopMode = useMediaQuery({ query: "(min-width: 1300px)" });
+
+  useEffect(() => {
+    console.log(destopMode);
+  });
+
   return <div>HomeHeader</div>;
 };
 export default HomeHeader;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,7 +10,7 @@ import HomeTeam from "./_components/team";
 
 export default function Home() {
   return (
-    <main>
+    <main className="max-w-[1920px] bg-white mx-auto relative overflow-hidden">
       <HomeHeader />
       <HomeHero />
       <HomeAbout />
