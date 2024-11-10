1b66ab7efcbb958773c7f7e5e49f54a06c46887a
Linda Paz <Lindacsndelapaz@gmail.com>
2024-11-10 15:12:44 -0300

    feat: fix text

--- src/app/_components/about.tsx
+++ src/app/_components/about.tsx
@@ -12,33 +12,30 @@ const HomeAbout = () => {
         sostenible
       </h2>
       <div className="w-1/3 bg-accent h-1"></div>
-      <div className="lg:hidden">
-        <p className="p py-4">
+      <div className="p py-4">
+        {/* Texto visible en pantallas pequeñas (sm) */}
+        <p className="p">
           Somos una firma independiente especializada en la identificación y
           <span className="span"> desarrollo de proyectos </span> innovadores
           que hacen realidad la Transición Energética{" "}
-          <span className="span underline">ver más</span>
-        </p>
-      </div>
-
-      <div className="hidden lg:block">
-        <p className="p py-4">
-          Somos una firma independiente especializada en la identificación y
-          <span className="span"> desarrollo de proyectos </span> innovadores
-          que hacen realidad la Transición Energética. Disponemos de un carácter
-          pionero y una predisposición a la innovación en todos los ámbitos del
-          sector, creando valor para nuestros clientes a través de nuestra
-          especialización y capacitación
+          <span className="span underline lg:hidden">ver más</span>
         </p>
 
-        <ol className="flex flex-col gap-3 mb-4 p">
+        {/* Texto adicional visible solo en pantallas grandes (lg) */}
+        <p className="hidden lg:block p">
+          Disponemos de un carácter pionero y una predisposición a la innovación
+          en todos los ámbitos del sector, creando valor para nuestros clientes
+          a través de nuestra especialización y capacitación.
+        </p>
+
+        <ol className="hidden lg:flex flex-col gap-3 my-4 p">
           <li className="span flex gap-2">
             <Check />
             <p>Expertos en proyectos energéticos</p>
           </li>
           <li className="span flex gap-2">
             <Check />
-            <p>Equipo interdisciplinario </p>
+            <p>Equipo interdisciplinario</p>
           </li>
           <li className="span flex gap-2">
             <Check />
