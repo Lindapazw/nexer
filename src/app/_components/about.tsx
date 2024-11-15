66ffeb16a6742b2ef2fce9ec828e25334e3248ad
Linda Paz <Lindacsndelapaz@gmail.com>
2024-11-15 20:31:32 -0300

    fix dialog in about section

--- src/app/_components/about.tsx
+++ src/app/_components/about.tsx
@@ -1,4 +1,12 @@
 "use client";
+import { DialogHeader } from "@/components/ui/dialog";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogTitle,
+  DialogTrigger,
+} from "@radix-ui/react-dialog";
 import { Check } from "lucide-react";
 import Image from "next/image";
 
@@ -14,21 +22,26 @@ const HomeAbout = () => {
           sostenible
         </h2>
         <div className="h-1 w-1/3 bg-accent"></div>
-        <div className="p mt-8">
-          <p className="p">
+        <div className="mt-8">
+          <p className="p mb-4 lg:hidden">
             Somos una firma independiente especializada en la identificación y
             <span className="span"> desarrollo de proyectos </span> innovadores
             que hacen realidad la Transición Energética{" "}
           </p>
 
-          <p className="p">
+          <p className="p hidden lg:block">
+            Somos una firma independiente especializada en la identificación y
+            <span className="span"> desarrollo de proyectos </span> innovadores
+            que hacen realidad la Transición Energética{" "}
+          </p>
+          <p className="p hidden lg:block">
             Disponemos de un carácter pionero y una predisposición a la
             innovación en todos los ámbitos del sector, creando valor para
             nuestros clientes a través de nuestra especialización y
             capacitación.
           </p>
-
-          <ol className="p my-4 flex-col gap-3 lg:flex xl:my-6">
+          {/* Lista para pantallas grandes */}
+          <ol className="p my-4 hidden flex-col gap-3 lg:flex xl:my-6">
             <li className="span flex gap-2">
               <Check />
               <p>Expertos en proyectos energéticos</p>
@@ -47,9 +60,48 @@ const HomeAbout = () => {
             </li>
           </ol>
         </div>
+        <div className="mb-8 lg:hidden">
+          <Dialog>
+            <DialogTrigger>
+              <p className="font-bold underline">Ver más</p>
+            </DialogTrigger>
+            <DialogContent>
+              <DialogHeader>
+                <DialogTitle>
+                  <h3 className="mt-4 text-start">
+                    Disponemos de un carácter pionero y una predisposición a la
+                    innovación en todos los ámbitos del sector, creando valor
+                    para nuestros clientes a través de nuestra especialización y
+                    capacitación.
+                  </h3>
+                </DialogTitle>
+
+                <DialogDescription>
+                  <ol className="p my-4 flex-col gap-3">
+                    <li className="span flex gap-2">
+                      <Check />
+                      <p>Expertos en proyectos energéticos</p>
+                    </li>
+                    <li className="span flex gap-2">
+                      <Check />
+                      <p>Equipo interdisciplinario</p>
+                    </li>
+                    <li className="span flex gap-2">
+                      <Check />
+                      <p>Soluciones sostenibles</p>
+                    </li>
+                    <li className="span flex gap-2">
+                      <Check />
+                      <p>Multitecnología</p>
+                    </li>
+                  </ol>
+                </DialogDescription>
+              </DialogHeader>
+            </DialogContent>
+          </Dialog>
+        </div>
       </div>
 
-      {/* Imagen o Espacio al final */}
       <div className="relative flex-grow">
         <Image
           src="/images/about_image.jpg"
