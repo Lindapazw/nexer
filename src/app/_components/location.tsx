76122c6ea951b56f62131071468a7771872b99f3
Linda Paz <Lindacsndelapaz@gmail.com>
2024-11-17 15:59:00 -0300

    feat: add location, hour and phone icons

--- src/app/_components/location.tsx
+++ src/app/_components/location.tsx
@@ -1,4 +1,7 @@
-import { LocateFixed, Mail, Phone, TimerIcon } from "lucide-react";
+import Hour from "./_icons.tsx/hour";
+import Location from "./_icons.tsx/location";
+import Mail from "./_icons.tsx/mail";
+import Phone from "./_icons.tsx/phone";
 
 const HomeLocation = () => {
   return (
@@ -14,36 +17,40 @@ const HomeLocation = () => {
       />
 
       {/* Contenido */}
-      <div className="container relative z-10 flex h-full w-full flex-col items-center justify-center py-6">
-        <h2 className="h2 text-white">Nuestra oficina</h2>
+      <div className="relative z-10 flex h-full w-full flex-col items-center justify-center py-6">
+        <div className="container">
+          <h2 className="h2 text-center text-white">Nuestra oficina</h2>
 
-        <div className="flex w-full justify-center">
-          <div className="h-1 w-1/3 bg-white"></div>
+          <div className="flex w-full justify-center">
+            <div className="h-1 w-1/3 bg-white"></div>
+          </div>
+
+          <p className="p mt-6 text-start text-white md:mt-12">
+            Contáctanos para saber más sobre nuestros servicios y cómo podemos
+            ayudarte a impulsar un futuro sostenible.
+          </p>
+
+          <ol className="p my-4 flex w-full flex-col gap-4">
+            <li className="span flex gap-2 text-white">
+              <Location className="h-5 w-5" />
+              <p>Avenida de Bruselas, 31. 28108 Alcobendas, Madrid, España</p>
+            </li>
+            <li className="span flex gap-2 text-white">
+              <Hour className="h-5 w-5" />
+              <p>Lunes - Viernes 09:00 - 18:00</p>
+            </li>
+            <li className="span flex gap-2 text-white">
+              <Phone className="h-6 w-6" />
+              <p>+34 911 68 12 19</p>
+            </li>
+            <li className="span flex gap-2 text-white">
+              <Mail className="h-5 w-5" />
+              <p>info@nexer.es</p>
+            </li>
+          </ol>
         </div>
 
-        <p className="p mt-6 text-start text-white md:mt-12">
-          Contáctanos para saber más sobre nuestros servicios y cómo podemos
-          ayudarte a impulsar un futuro sostenible.
-        </p>
-
-        <ol className="p my-4 flex w-full flex-col gap-4">
-          <li className="span flex gap-2 text-white">
-            <LocateFixed />
-            <p>Avenida de Bruselas, 31. 28108 Alcobendas, Madrid, España</p>
-          </li>
-          <li className="span flex gap-2 text-white">
-            <TimerIcon />
-            <p>Lunes - Viernes 09:00 - 18:00</p>
-          </li>
-          <li className="span flex gap-2 text-white">
-            <Phone />
-            <p>+34 911 68 12 19</p>
-          </li>
-          <li className="span flex gap-2 text-white">
-            <Mail />
-            <p>info@nexer.es</p>
-          </li>
-        </ol>
+        <div className="min-h-[25vh] w-full bg-red-400">mapa</div>
       </div>
     </div>
   );
