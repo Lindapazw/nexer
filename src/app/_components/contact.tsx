f169247aa35365f4dcf325cb0b47eb6f0ae54848
Linda Paz <Lindacsndelapaz@gmail.com>
2024-12-02 13:11:31 -0300

    feat: add contact motion

--- src/app/_components/contact.tsx
+++ src/app/_components/contact.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { Button } from "@/components/ui/button";
 import {
   Dialog,
@@ -5,6 +7,7 @@ import {
   DialogTitle,
   DialogTrigger,
 } from "@/components/ui/dialog";
+import { motion } from "framer-motion";
 import { Clock3, Mail, MapPin, Phone } from "lucide-react";
 import ContactForm from "./contact-form";
 import Map from "./map";
@@ -37,14 +40,34 @@ const HomeContact = () => {
     <section className="container relative mx-auto flex min-h-[calc(100vh-3.25rem)] flex-col items-center justify-center py-6">
       <div id="contact" className="absolute -top-[3.25rem] h-[3.25rem]"></div>
 
-      <h2 className="h2 text-center">Contáctanos</h2>
+      <motion.h3
+        className="h2 text-center text-black md:mt-5"
+        initial={{ opacity: 0, y: 70, scale: 0.9 }}
+        whileInView={{ opacity: 1, y: 0, scale: 1 }}
+        viewport={{ once: true, amount: 0.5 }}
+        transition={{ duration: 0.8, ease: "easeOut" }}
+      >
+        Contáctanos
+      </motion.h3>
 
-      <p className="p text-center">
-        Para saber más sobre nuestros servicios y cómo podemos
-        ayudarte a impulsar un futuro sostenible.
-      </p>
+      <motion.p
+        className="p mb-4 text-black"
+        initial={{ opacity: 0, y: 70, scale: 0.9 }}
+        whileInView={{ opacity: 1, y: 0, scale: 1 }}
+        viewport={{ once: true, amount: 0.5 }}
+        transition={{ duration: 1, ease: "easeOut", delay: 0.3 }}
+      >
+        Para saber más sobre nuestros servicios y cómo podemos ayudarte a
+        impulsar un futuro sostenible.
+      </motion.p>
 
-      <div className="mt-12 flex flex-col items-center gap-12 lg:flex-row lg:gap-20">
+      <motion.div
+        className="mt-12 flex flex-col items-center gap-12 lg:flex-row lg:gap-20"
+        initial={{ opacity: 0, y: 70, scale: 0.9 }}
+        whileInView={{ opacity: 1, y: 0, scale: 1 }}
+        viewport={{ once: true, amount: 0.5 }}
+        transition={{ duration: 1, ease: "easeOut", delay: 0.3 }}
+      >
         <ContactForm />
 
         <article className="flex flex-col gap-6 rounded-xl bg-accent px-8 py-6 text-white">
@@ -77,7 +100,7 @@ const HomeContact = () => {
             </DialogContent>
           </Dialog>
         </article>
-      </div>
+      </motion.div>
     </section>
   );
 };
